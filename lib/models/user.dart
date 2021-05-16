4964fe76e3c499d27e27e9292ec6fe46ae45c30a
dandy <danielsbarbosa27@gmail.com>
2021-05-16 02:48:28 -0300

    Salvando os dados do usuario

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,31 @@
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
-User({this.email, this.password, this.name, this.confirmPassword});
+User({this.email, this.password, this.name, this.confirmPassword, this.id});
+
+  String id;
   String email;
   String password;
   String name;
 
   String confirmPassword;
+
+  DocumentReference get firestoreRef =>
+  Firestore.instance.document('users/$id');
+
+
+  Future<void> saveData()async{
+   await  firestoreRef.setData(toMap());
+  }
+
+  Map<String, dynamic> toMap(){
+return {
+  'name': name,
+  'email': email,
+};
+  }
 
 }
